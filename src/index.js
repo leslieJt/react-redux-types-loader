b27e64d1852b98b367681ac076ae3b810d7911ae
石坚 <574745389@qq.com>
2017-11-10 01:03:31 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-const {transform} = require('babel-core');
+const { transform } = require('babel-core');
 const plugin = require('./plugin');
 
-module.exports = (source)=>{
-    const {code,map,ast} = transform(source,{plugins: [plugin]});
-    return code;
-}
+module.exports = function(source) {
+  const path = this.context;
+  const { code, map, ast } = transform(source, {
+    plugins: [[plugin, { module_path: path }]]
+  });
+  return code;
+};
