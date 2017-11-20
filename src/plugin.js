9375dc55993c779d9f876893507fb4be4d5856c9
石坚 <574745389@qq.com>
2017-11-20 10:03:42 +0800

    Add files via upload

--- src/plugin.js
+++ src/plugin.js
@@ -9,13 +9,17 @@ module.exports = function({ types: t }) {
         if (!declaration.get('declarations').length) return;
         declaration.get('declarations').map((item, index) => {
           if (item.isVariableDeclarator()) {
+            let value = item.get('init').node
+              ? item.get('init').node.value
+              : '';
+            let str =
+              (module_path ? module_path : +new Date()) +
+              '/' +
+              (value ? value.toString() : item.get('id').node.name);
+            let cwd = process.cwd();
             item
               .get('init')
-              .replaceWith(
-                t.StringLiteral(
-                  (module_path ? module_path : +new Date()) + '_' + index
-                )
-              );
+              .replaceWith(t.StringLiteral(str.replace(new RegExp(cwd), '')));
           }
         });
       }
